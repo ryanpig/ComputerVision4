248457bdeeeb5417f90aab99490fbda6ce3127c4
ryanpig <ryanpig@gmail.com>
2018-03-23 14:11:37 +0100

    3/21 [readfunc_v2] - clean the code. Add snippest to read testing data. [ReadVideo_film] - To pick up frame from recorded video. (Same as ReadVideo.) [cnn_main_v2] - accuracy (Tr:65, Ev:60, Te:40) [data_aug] & [data_aug_translation] - data augmentation (flip, reduce, crop, resize, contrast, brightness, translation)

--- cnn_main_v2.py
+++ cnn_main_v2.py
@@ -1,12 +1,38 @@
 # version 2
-# to build a 3ch CNN model
-#from readfunc import ReadData
-from readfunc_v2 import readData
 
 import numpy as np
 import tensorflow as tf
+from sklearn.model_selection import KFold
+from readfunc_v2 import single_dataset_gen
+from readfunc_v2 import examples
+from readfunc_v2 import kfold_dataset_gen
+
 
 tf.logging.set_verbosity(tf.logging.INFO)
+def eval_confusion_matrix(labels, predictions):
+    with tf.variable_scope("eval_confusion_matrix"):
+        con_matrix = tf.confusion_matrix(labels=labels, predictions=predictions, num_classes=5)
+
+        con_matrix_sum = tf.Variable(tf.zeros(shape=(5,5), dtype=tf.int32),
+                                            trainable=False,
+                                            name="confusion_matrix_result",
+                                            collections=[tf.GraphKeys.LOCAL_VARIABLES])
+
+
+        update_op = tf.assign_add(con_matrix_sum, con_matrix)
+
+        return tf.convert_to_tensor(con_matrix_sum), update_op
+
+def cal_p_r_fscore(conf):
+    FP = np.sum(conf, axis=0) - np.diag(conf)
+    FN = np.sum(conf, axis=1) - np.diag(conf)
+    TP = np.diag(conf)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    print("Precision:", precision)
+    print("Recall:", recall)
+    fscore = 2 * precision * recall / (precision + recall)
+    print("F-Score:", fscore)
 
 def cnn_model_fn(features, labels, mode):
   # in,c1,p1,c2,p2,flat,dense,drop,logits
@@ -50,105 +76,163 @@ def cnn_model_fn(features, labels, mode):
         loss=loss,
         global_step=tf.train.get_global_step())
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
   # Add evaluation metrics (for EVAL mode)
-  m_confusion = tf.confusion_matrix(labels,predictions["classes"], 5)
 
   eval_metric_ops = {
       "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
+          labels=labels, predictions=predictions["classes"]),
+      "conf_matrix": eval_confusion_matrix(
+          labels, predictions["classes"])
+  }
 
-
-  # predi = predictions["classes"] , one batch results in two predictions [3 3]
-  # Save them and make confusion_matrix, outside of function.
-
-  #comfusion = tf.confusion_matrix(labels=labels,predictions=predi,
-                                  #num_classes=5,name='confusionMat')
   return tf.estimator.EstimatorSpec(
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
+def gen_input_fn(features, labels, bs=32, ep=2, sh=True):
+    input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": features},
+        y=labels,
+        batch_size=bs,
+        num_epochs=ep,
+        shuffle=sh)
+    return input_fn
 
-#def main(unused_argv):
-# Load training and eval data
-'''
-un = 1
-train1, test1 = ReadData(un)
-train_data = train1.images
-train_labels = np.asarray(train1.labels, dtype=np.int32)
-eval_data = test1.images
-eval_labels = np.asarray(test1.labels, dtype=np.int32)
-'''
-train1,eval1, test1 = readData()
-train_data = train1.images
-train_labels = train1.labels
-eval_data = eval1.images
-eval_labels = eval1.labels
-test_data = test1.images
-test_labels =  test1.labels
+def kfold_cross_validation(data):
+    kf = KFold(n_splits=10)
+    for train_ind, eval_ind in kf.split(data):
+        train_d = examples(len(train_ind))
+        evaluate_d = examples(len(eval_ind))
+        train_d.images = data.images[train_ind]
+        train_d.labels = data.labels[train_ind]
+        evaluate_d.images = data.images[eval_ind]
+        evaluate_d.labels = data.labels[eval_ind]
+        return train_d, evaluate_d
 
 
-print(np.shape(train_data))
-print(np.shape(eval_data))
-print(train_labels)
-
+# --------------- Main Processing ----------------
 # Create the Estimator
+print("Estimator Creating...")
 action_classifier = tf.estimator.Estimator(
-  model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model9")
+  model_fn=cnn_model_fn, model_dir="/tmp/cnn_model_no_data_aug")
+  #model_fn=cnn_model_fn, model_dir="/tmp/cnn_model_data_aug")
 
 # Set up logging for predictions
 # Log the values in the "Softmax" tensor with label "probabilities"
+print("Training Logging Setting...")
 tensors_to_log = {"probabilities": "softmax_tensor",
                 "classes": "predication_classes"}
 logging_hook = tf.train.LoggingTensorHook(
   tensors=tensors_to_log, every_n_iter=50)
 
-# Train the model
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-  x={"x": train_data},
-  y=train_labels,
-  batch_size=2,
-  num_epochs=2,
-  shuffle=True)
-action_classifier.train(
-  input_fn=train_input_fn,
-  steps=10, #20000
-  hooks=[logging_hook])
+# Load training, evaluation, and testing data
+Flag_Once_Generate_Dateset = False
 
-# Evaluate the model by training data
-eval_Tr_input_fn = tf.estimator.inputs.numpy_input_fn(
-  x={"x": train_data},
-  y=train_labels,
-  batch_size=32,
-  num_epochs=2,
-  shuffle=True)
-eval_Tr_results = action_classifier.evaluate(input_fn=eval_Tr_input_fn)
-print("Evaluate Training data:", eval_Tr_results)
+if Flag_Once_Generate_Dateset == True:
+    print("Generate single dataset..")
+    train1,eval1, test1 = single_dataset_gen()
+    train_data = train1.images
+    train_labels = train1.labels
+    eval_data = eval1.images
+    eval_labels = eval1.labels
+    test_data = test1.images
+    test_labels =  test1.labels
+    print("Get Training data:", np.shape(train_data))
+    print("Get Evaluating data:", np.shape(eval_data))
+    print("Get Testing data:", np.shape(test_data))
+    print(train_labels)
 
-# Evaluate the model by evaluating data
-eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-  x={"x": eval_data},
-  y=eval_labels,
-  batch_size=32,
-  num_epochs=2,
-  shuffle=True)
-eval_results = action_classifier.evaluate(input_fn=eval_input_fn)
-print("Evaluate Evaluating data:",eval_results)
 
-# Testing the model by filmed video.
-test_input_fn = tf.estimator.inputs.numpy_input_fn(
-  x={"x": test_data},
-  y=test_labels,
-  batch_size=1,
-  num_epochs=2,
-  shuffle=True)
-test_results = action_classifier.evaluate(input_fn=test_input_fn)
-print("Evaluate Testing data:",test_results)
 
+# Evaluation both training and evaluation data once
+Flag_Once_Evaluation = False
+
+if Flag_Once_Evaluation == True:
+    print("--Evaluation once start--")
+    eval_Tr_input_fn = gen_input_fn(train_data, train_labels, bs=32, ep=2, sh=False)
+    eval_Tr_results = action_classifier.evaluate(input_fn=eval_Tr_input_fn)
+    print("-----------------------")
+    print("Evaluate Training data result:", eval_Tr_results)
+
+    eval_input_fn = gen_input_fn(eval_data, eval_labels, bs=32, ep=2, sh=False)
+    eval_results = action_classifier.evaluate(input_fn=eval_input_fn)
+    # print precision, recall and fscore
+    cal_p_r_fscore(eval_Tr_results['conf_matrix'])
+    print("-----------------------")
+    print("Evaluate Evaluating data result:", eval_results)
+    cal_p_r_fscore(eval_results['conf_matrix'])
+    print("--Evaluation once end--")
+# Cross Validation
+Flag_Cross_Validation = True
+
+if Flag_Cross_Validation == True:
+    data = kfold_dataset_gen()
+    kf = KFold(n_splits=10,shuffle=True)
+    count = 0
+    total_acc = []
+    model_dir = "/tmp/cnn_model_cv"
+    print("10-fold cross validation starts...")
+    print("Before spliting:",len(data.labels))
+    for tr_ind, ev_ind in kf.split(data.labels):
+        print(len(tr_ind),len(ev_ind))
+        tr = examples(len(tr_ind))
+        ev = examples(len(ev_ind))
+        tr.images = data.images[tr_ind]
+        tr.labels = data.labels[tr_ind]
+        ev.images = data.images[ev_ind]
+        ev.labels = data.labels[ev_ind]
+        # Create a new model
+        model_dir = model_dir + '_' + str(count)
+        action_classifier = tf.estimator.Estimator(
+            model_fn=cnn_model_fn, model_dir=model_dir)
+        train_input_fn = gen_input_fn(tr.images, tr.labels, bs=32, ep=2, sh=False)
+        # Train & Evaluate
+        print("Model:", count, "Training start")
+        action_classifier.train(
+            input_fn=train_input_fn, steps=800, hooks=[logging_hook])
+        eval_input_fn = gen_input_fn(ev.images, ev.labels, bs=32, ep=2, sh=False)
+        eval_results = action_classifier.evaluate(input_fn=eval_input_fn)
+        print("Evaluate result:", eval_results)
+        cal_p_r_fscore(eval_results['conf_matrix'])
+        total_acc.append(eval_results['accuracy'])
+        print(ev_ind)
+        count += 1
+    print("Average Acc:", sum(total_acc) / len(total_acc))
+
+# Long 2500 steps Training
+Flag_Long_Training = False
+
+if Flag_Long_Training == True:
+    acc_train = []
+    acc_eval = []
+    acc_test = []
+    loss_train = []
+    print("long long long training starts...")
+    for i in range(50):
+        # Train the model
+        train_input_fn = gen_input_fn(train_data, train_labels, bs=32, ep=2, sh=False)
+        action_classifier.train(
+          input_fn=train_input_fn,
+          steps=50, #20000
+          hooks=[logging_hook])
+
+        # Evaluate training data
+        eval_Tr_input_fn =gen_input_fn(train_data, train_labels, bs=32, ep=2, sh=False)
+        eval_Tr_results = action_classifier.evaluate(input_fn=eval_Tr_input_fn)
+        print("Evaluate Training data:", eval_Tr_results)
+        # Evaluate evaluating data
+        eval_input_fn =  gen_input_fn(eval_data, eval_labels, bs=32, ep=2, sh=False)
+        eval_results = action_classifier.evaluate(input_fn=eval_input_fn)
+        print("Evaluate Evaluating data:",eval_results)
+        # log
+        acc_eval.append(eval_results['accuracy'])
+        acc_train.append(eval_Tr_results['accuracy'])
+        loss_train.append((eval_Tr_results['loss']))
+        # Testing the model by filmed video.
+        test_input_fn = gen_input_fn(test_data, test_labels, bs=1, ep=2, sh=True)
+        test_results = action_classifier.evaluate(input_fn=test_input_fn)
+        print("Evaluate Testing data:",test_results)
+        acc_test.append(test_results['accuracy'])
 
 # The shape of each layer in CNN model
 #for tmp in  action_classifier.get_variable_names():
 #    print(np.shape(action_classifier.get_variable_value(name=tmp)))
-
-
-#if __name__ == "__main__":
-#  tf.app.run()
--- readfunc_v2.py
+++ readfunc_v2.py
@@ -1,9 +1,19 @@
+###
+# readData(): Read images and return train, eval, and test data sets
+# labeling(): Labeling each image based on the filename
+# tf_distort_images() in data_aug.py: Data augmentation
+# -> flip, reduce, crop, resize, contrast, brightness, translation
+# dataDivision() : Divide data into training & evaluation
+# Process flow: readData -> labeling -> dataDivision -> tf_distort_images
 
 import tensorflow as tf
 import glob
 import cv2
 import numpy as np
+
 from data_aug import tf_distort_images
+Flag_data_aug = False
+
 
 # Configuration
 Division_ratio = 0.8 # the data ratio for training and evaluating data.
@@ -13,10 +23,29 @@ class examples:
     def __init__(self, len):
         self.labels = np.zeros(len, dtype=np.int32)
         self.images = np.zeros([len,120,120,3], dtype=np.float32)
+
 def normalize(arr):
     if arr.max() > 1.0:
         arr/=255.0
     return arr
+def single_dataset_gen():
+    # Read data and labeling
+    traindata, test_d = readData()
+    # Divide data into training and evaluating data
+    train_d, eval_d = dataDivision(traindata, Division_ratio)
+    # Data Augmentation (Double training data:496 -> 992)
+    if Flag_data_aug == True:
+        train_d = data_augmentation(train_d)
+    print("Training examples:", len(train_d.labels),
+          "Evaluate examples:", len(eval_d.labels),
+          "Testing examples:", len(test_d.labels))
+    return train_d, eval_d, test_d
+
+# Only return train & eval data.
+def kfold_dataset_gen():
+    # Read data and labeling
+    traindata, test_d = readData()
+    return traindata
 
 def readData():
     # Get file lists from both training and testing folders
@@ -30,13 +59,8 @@ def readData():
     # labeling and reading features
     data = labeling(filelist)
     test_d = labeling(filelist_Test)
-    # Divide data into training and evaluating data
-    train_d, eval_d = dataDivision(data, Division_ratio)
-    print("testing examples:",len(test_d.labels))
-    # Data Augmentation
-    # tmp = tf_distort_images(train_d.images)
-    # train_d.images =
-    return train_d, eval_d, test_d
+    return data, test_d
+
 # labeling based on the file name.
 def labeling(filelist):
     lenF = len(filelist)
@@ -59,7 +83,7 @@ def dataDivision(data, division):
     total_len = len(data.labels)
     tr_len = int(total_len * division)
     te_len = total_len - tr_len
-    print("training examples:", tr_len, "- evaluate examples:", te_len)
+
     # initialize train & eval
     train = examples(tr_len)
     evaluate = examples(te_len)
@@ -75,12 +99,36 @@ def dataDivision(data, division):
         evaluate.labels[k] = data.labels[ind]
         evaluate.images[k] = data.images[ind]
     return train, evaluate
+def data_augmentation(train_d):
+    distorted_images = tf_distort_images(train_d.images)
+    normalize(distorted_images)
+    train_d.images = np.concatenate((train_d.images, distorted_images), axis=0)
+    train_d.labels = np.concatenate((train_d.labels, train_d.labels), axis=0)
+    return train_d
 
+def debug():
+    train1,eval1, test1 = readData()
+    train_data = train1.images
+    train_labels = train1.labels
+    eval_data = eval1.images
+    eval_labels = eval1.labels
+    test_data = test1.images
+    test_labels =  test1.labels
 
-train1,eval1, test1 = readData()
-train_data = train1.images
-train_labels = train1.labels
-eval_data = eval1.images
-eval_labels = eval1.labels
-test_data = test1.images
-test_labels =  test1.labels
+'''
+X = np.arange(20).reshape(10,2)
+y = np.arange(10)
+kf = KFold(n_splits=10) ## 20/4 = 5 for each fold, so partition is (15,5)
+print(X)
+count = 0
+for train_ind, test_ind in kf.split(X):
+    count = count + 1
+    print(count)
+    print("t_ind", train_ind)
+    print("te_ind",test_ind)
+    print(X[train_ind])
+    print(y[train_ind])
+    print("--")
+    print(X[test_ind])
+    print(y[test_ind])
+'''
